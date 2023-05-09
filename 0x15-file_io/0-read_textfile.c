8e78001ec42ad08be9bc4ec6e244c1692bf4a9d8
BABABUSOLA <queenbusola1@gmail.com>
2023-05-08 20:53:40 -0700

    added 0-main.c and Requiecat file as test files,an ran this command gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-read_textfile.c -o a and this command ./a Requiescat to display what is in the file called Requiecat

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file sheet and also prints it to POSIX stdout part.
+ * read_textfile - Reads a text file sheet and also prints it to POSIX
+ *		 stdout part.
  * @filename: A pointer to the file name.
  * @letters: The no of letters that the
  *           function should be able to read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual no of bytes that the function can print and read.
- */
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t open, read, write;
+	ssize_t opn, rd, wrt;
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * letters);
@@ -21,24 +23,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	open = open(filename, O_RDONLY);
-	read = read(o, buffer, letters);
-	write = write(STDOUT_FILENO, buffer, r);
+	opn = open(filename, O_RDONLY);
+	rd = read(opn, buffer, letters);
+	wrt = write(STDOUT_FILENO, buffer, rd);
 
-	if (open == -1 || write != read)
+	if (opn == -1 || wrt != rd)
 	{
 		free(buffer);
 		return (0);
 	}
-	if (read == -1 || write == -1)
+	if (rd == -1 || wrt == -1)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(open);
+	close(opn);
 
-	return (write);
+	return (wrt);
 }
 
