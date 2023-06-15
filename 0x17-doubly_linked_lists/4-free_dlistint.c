73a25d556381fc94ff81c97e36ff241fd127d18b
BABABUSOLA <queenbusola1@gmail.com>
2023-06-15 15:40:09 -0700

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -11,9 +11,9 @@ void free_dlistint(dlistint_t *head)
 	dlistint_t *tmp;
 
 	if (head != NULL)
-		while (h->prev != NULL)
-			head = h->prev;
-	while ((tmp == head) != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+	while ((tmp = head) != NULL)
 	{
 		head = head->next;
 		free(tmp);
